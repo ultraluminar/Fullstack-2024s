3e5e00bba67851d898f2fd80cf3668283d13cdb3
KlasL4243 <lukasklassen4243@gmail.com>
2024-04-24 11:50:53 +0200

    Update customer.js to use PostgreSQL as the database and add database connection validation

--- customer.js
+++ customer.js
@@ -2,6 +2,14 @@ import { Sequelize, DataTypes } from "sequelize";
 
 const seq = new Sequelize('customerdb', 'root', 'mariadb', {
     host: 'localhost', 
+    dialect: 'postgres',
+    //dialectOptions: {connnectTimeout: 10000},
+    //pool: {max: 5, min: 0, acquire: 30000, idle: 10000}
 });
 
-
+try {
+    await seq.authenticate();
+    console.log('Connection has been established successfully.');
+} catch (error){
+    console.error('Unable to connect to database: ', error);
+}
