488843acc704ae067ab512291fc90c3dc2edd3de
KlasL4243 <lukasklassen4243@gmail.com>
2024-04-25 22:55:49 +0200

    Add user controller functions and express request handlers

--- server-seq/server.js
+++ server-seq/server.js
@@ -1,46 +1,32 @@
 import express, { request, response } from 'express';
-//import { pool } from './db.js';
+import bodyParser from 'body-parser';
 
-import { sequelize } from './db.js';
-import './userModel.js';
+import { getAllUsers, getCustomerByID, addCustomer } from './controllers/user.js';
 
 const app = express();
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.json());
 
-/*
+
 app.get('/', async (request, response) => {
     try {
-        const data = await pool.query('SELECT * FROM dbtable');
-        response.status(200).send(data.rows);
+        await getAllUsers(request, response);
     } catch (error){
-        console.log(error);
-        response.sendStatus(500);
+        console.log('Unable to GET: ', error);
     }
 });
+
+/* TODO: getCustomerByID */
 
 app.post('/', async (request, response) => {
-    const user = request.body.user;
-    const password = request.body.password;
-
     try {
-        await pool.query('INSERT INTO dbtable ("user", "password") VALUES ($1, $2)', [user, password]);
-        response.status(200).send(`Successfully added user ${user}.`);
-    } catch (error){
-        console.log(error);
-        response.sendStatus(500);
+        await addCustomer(request, response);
+    } catch (error) {
+        console.log('Unable to add User', error);
     }
 });
 
-app.get('/setup', async (request, response) => {
-    try {
-        await pool.query('CREATE TABLE dbtable (id SERIAL PRIMARY KEY, "user" VARCHAR(100), "password" VARCHAR(100))');
-        response.status(200).send(`Successfully created table: dbtable.`);
-    } catch (error){
-        console.log(error);
-        response.sendStatus(500);
-    }
-});
-*/
+
 app.listen(process.env.SERVER_PORT_INTERN, () => {
     console.log(`Server listening on port: ${process.env.SERVER_PORT_INTERN}.`)
 });
