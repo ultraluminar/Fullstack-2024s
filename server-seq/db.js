35ebc00b0c404b28ab08671eb65284ba49b0666d
KlasL4243 <lukasklassen4243@gmail.com>
2024-04-25 19:31:33 +0200

    Using Sequelize for db connection

--- server-seq/db.js
+++ server-seq/db.js
@@ -1,19 +1,23 @@
-import pg from "pg";
-const { Pool } = pg;
+import { Sequelize } from 'sequelize';
 
-const pool = new Pool({
-    host: 'db-service',
-    port: 5432,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_DATABASE
-});
 
-export { pool }
+const sequelize = new Sequelize(
+    process.env.DB_DATABASE, 
+    process.env.DB_USER, 
+    process.env.DB_PASSWORD, 
+    {
+        dialect: 'postgres',
+        host: process.env.DB_HOST
+    }
+);
 
-import { Sequelize } from "sequelize";
 
-const sequelize = new Sequelize(process.env.DB_DATABASE, process.env.DB_USER, process.env.DB_PASSWORD, {
-    dialect: 'postgres',
-    host: process.env.DB_HOST
-});
+try {
+    await sequelize.authenticate();
+    console.log('Connection has been established successfully.');
+} catch(error){
+    console.error('Unable to connect to database: ', error);
+}
+
+
+export { sequelize }
