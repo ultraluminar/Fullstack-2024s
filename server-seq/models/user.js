6283055adf6db57031c3d7d0b8b8b4091d703507
Adrian Lösch <loeschadrianp+git@gmail.com>
2024-04-25 20:59:04 +0200

    Moved userModel into models folder with new name user

---
+++ server-seq/models/user.js
@@ -0,0 +1,32 @@
+import { DataTypes } from 'sequelize';
+import { sequelize } from './db.js';
+
+sequelize.define(
+    'User', 
+    {
+        id: {
+            type: DataTypes.INTEGER,
+            primaryKey: true,
+            allowNull: false
+        },
+        firstName: {
+            type: DataTypes.STRING,
+            allowNull: false
+        },
+        lastName: {
+            type: DataTypes.STRING,
+            allowNull: false
+        },
+        dateOfBirth: {
+            type: DataTypes.DATE,
+            allowNull: true
+        }
+    }
+);
+
+try {
+    await sequelize.sync();
+    console.log('Successfully synced models with database.');
+} catch (error) {
+    console.error('Unable to sync models: ', error);
+}
