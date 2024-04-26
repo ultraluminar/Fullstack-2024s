c37852eb1d45be79adbc256e691bf2df073e44fb
KlasL4243 <lukasklassen4243@gmail.com>
2024-04-26 13:18:57 +0200

    changed DB environment var names to POSTGRES

--- server-seq/db.js
+++ server-seq/db.js
@@ -2,12 +2,12 @@ import { Sequelize } from 'sequelize';
 
 
 const sequelize = new Sequelize(
-    process.env.DB_DATABASE, 
-    process.env.DB_USER, 
-    process.env.DB_PASSWORD, 
+    process.env.POSTGRES_DB, 
+    process.env.POSTGRES_USER, 
+    process.env.POSTGRES_PASSWORD, 
     {
         dialect: 'postgres',
-        host: process.env.DB_HOST
+        host: process.env.POSTGRES_HOST
     }
 );
 
