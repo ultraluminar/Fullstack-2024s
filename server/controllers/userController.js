64bd5346f82dbefec66907a2fe79f30de27c7f4d
KlasL4243 <lukasklassen4243@gmail.com>
2024-04-27 18:59:30 +0200

    Added json responses to all requests

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -4,7 +4,7 @@ import { getAll, get, add, remove, update } from "../orms/sequelize/models/userM
 async function getAllUsers(request, response){
     try {
         const users = await getAll();
-        response.status(200).json(users);
+        response.status(200).json({ users: users });
     } catch (error){
         console.log('Unable to get all Users\n', error);
         response.status(500).json({ error: 'Unable to get all Users' })
@@ -16,7 +16,7 @@ async function getUser(request, response){
         const id = request.params.id;
         const user = await get(id);
         if (user){
-            response.status(200).json(user);
+            response.status(200).json({ user: user });
         } else {
             response.status(404).json({ error: `User where id=${id} not Found` });
         }
@@ -30,7 +30,7 @@ async function addUser(request, response){
     try {
         const user_json = requestToUserJson(request);
         const user = await add(user_json);
-        response.status(201).json(user);
+        response.status(201).json({ success: `User where firstName=${user_json.firstName} added successfully` });
     } catch (error){
         console.log('Unable to add User\n', error);
         response.status(500).json({ error: 'Unable to add User' })
@@ -44,7 +44,7 @@ async function updateUser(request, response){
         const user = await get(id);
         if (user){
             await update(user_json, id);
-            response.status(200).json();
+            response.status(200).json({ success: `User where id=${id} updated successfully` });
         } else {
             response.status(404).json({ error: `User where id=${id} not Found` });
         }
@@ -60,7 +60,7 @@ async function deleteUser(request, response){
         const user = await get(id);
         if (user){
             await remove(id)
-            response.status(200).json();
+            response.status(200).json({ success: `User where id=${id} deleted successfully` });
         } else {
             response.status(404).json({ error: `User where id=${id} not Found` });
         }
