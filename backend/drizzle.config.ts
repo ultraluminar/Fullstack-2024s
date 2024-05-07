cbe4ef45f3cb51260f421b3f980fd3358af8f656
Adrian Lösch <loeschadrianp+git@gmail.com>
2024-05-07 19:11:51 +0200

    chore: add database migration files and configuration

---
+++ backend/drizzle.config.ts
@@ -0,0 +1,14 @@
+import type { Config } from 'drizzle-kit';
+
+export default {
+    schema: './db/schema.ts',
+    out: './db/migrations/',
+    driver: 'pg',
+    verbose: true,
+    dbCredentials: {
+        host: process.env.POSTGRES_HOST!,
+        user: process.env.POSTGRES_USER!,
+        password: process.env.POSTGRES_PASSWORD!,
+        database: process.env.POSTGRES_DB!
+    }
+} satisfies Config;
