c36269ff69a8afd4b1297f0c9c8acb10fa3c159a
Adrian Lösch <loeschadrianp+git@gmail.com>
2024-05-07 18:15:42 +0200

    fix: change eslint config to work with typescript and esmodule

--- backend/eslint.config.js
+++ backend/eslint.config.js
@@ -6,11 +6,6 @@ import tseslint from "typescript-eslint";
 export default [
     {
         languageOptions: { globals: globals.browser },
-        rules: {
-            eqeqeq: ["error", "always"],
-            camelcase: ["error", { properties: "always" }],
-            semi: ["error", "always"],
-        }
     },
     pluginJs.configs.recommended,
     ...tseslint.configs.recommended,
