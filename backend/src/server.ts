33d62cb35b0157e47f00001db287dd01ff020dda
Adrian Lösch <loeschadrianp+git@gmail.com>
2024-05-14 15:36:27 +0200

    feat: use new routers

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,14 +1,23 @@
 import express from "express";
 import bodyParser from "body-parser";
 
-import { router } from "./routes/routes.js";
+import { accountRouter } from './routes/accountRouter.js';
+import { stockRouter } from './routes/stockRouter.js';
+import { orderRouter } from './routes/orderRouter.js';
 
 const app = express();
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.json());
 
-app.use('/', router);
+app.use('/account/', accountRouter);
+app.use('/account/:id', accountRouter);  // TODO: test if this extra statement is needed
+
+app.use('/stock/', stockRouter);
+app.use('/stock/:id', stockRouter);
+
+app.use('/order/', orderRouter);
+app.use('/order/:id', orderRouter);
 
 app.listen(process.env.BACKEND_PORT_INTERNAL, () => {
     console.log(`Server is listening on port ${process.env.BACKEND_PORT_INTERNAL}`);
