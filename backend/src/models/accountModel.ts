333f25b84d7281582a3e49b8f9c927a0189e1660
KlasL4243 <lukasklassen4243@gmail.com>
2024-06-25 16:30:17 +0200

    chore: use more return types

--- backend/src/models/accountModel.ts
+++ backend/src/models/accountModel.ts
@@ -1,26 +1,42 @@
-import { eq } from 'drizzle-orm';
+import { eq, exists } from 'drizzle-orm';
 
 import { accounts, AccountType, NewAccountType } from '../../db/schema/account.js';
 import { db } from '../../db/db.js';
 
 export const Account = {
-    async getAll() {
-        return await db.select().from(accounts);
+    async getAll(): Promise<AccountType[]> {
+        return await db
+            .select()
+            .from(accounts);
     },
-    
-    async getById(id: AccountType['id']) {
-        return await db.select().from(accounts).where(eq(accounts.id, id));
+
+    async getById(id: AccountType['id']): Promise<AccountType[]> {
+        return await db
+            .select()
+            .from(accounts)
+            .where(eq(accounts.id, id));
     },
-    
-    async create(accountData: NewAccountType) {
-        return await db.insert(accounts).values(accountData);
+
+    async create(accountData: NewAccountType): Promise<AccountType[]> {
+        return await db
+            .insert(accounts)
+            .values(accountData)
+            .onConflictDoNothing({ target: accounts.id })
+            .returning();
     },
-    
-    async remove(id: AccountType['id']) {
-        return await db.delete(accounts).where(eq(accounts.id, id));
+
+    async remove(id: AccountType['id']): Promise<AccountType[]> {
+        return await db
+            .delete(accounts)
+            .where(eq(accounts.id, id))
+            .returning();
     },
-    
-    async update(id:AccountType['id'], accountData: NewAccountType) {
-        return await db.update(accounts).set(accountData).where(eq(accounts.id, id));
-    }
+
+    async update(id: AccountType['id'], accountData: NewAccountType): Promise<AccountType[]> {
+        return await db
+            .update(accounts)
+            .set(accountData)
+            .where(eq(accounts.id, id))
+            .returning();
+    },
 };
