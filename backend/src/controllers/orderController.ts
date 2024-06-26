d51097960dd1fd35947f684a1102c20ca2cfdcda
KlasL4243 <lukasklassen4243@gmail.com>
2024-06-26 16:36:19 +0200

    chore: add empty functions to satisfy tsc compiler

---
+++ backend/src/controllers/orderController.ts
@@ -0,0 +1,19 @@
+export function getAllOrders() {
+    throw new Error("not implemented");
+}
+
+export function getOrder() {
+    throw new Error("not implemented");
+}
+
+export function createOrder() {
+    throw new Error("not implemented");
+}
+
+export function removeOrder() {
+    throw new Error("not implemented");
+}
+
+export function updateOrder() {
+    throw new Error("not implemented");
+}
---
+++ backend/src/controllers/stockController.ts
@@ -0,0 +1,19 @@
+export function getAllStocks() {
+    throw new Error("not implemented");
+}
+
+export function getStock() {
+    throw new Error("not implemented");
+}
+
+export function createStock() {
+    throw new Error("not implemented");
+}
+
+export function removeStock() {
+    throw new Error("not implemented");
+}
+
+export function updateStock() {
+    throw new Error("not implemented");
+}
